19a8a3b8026eeb9aa2c621d7577cc689607c2e5a
Hamza <hzahir47@gmail.com>
2024-02-23 15:53:00 +0500

    added model

--- Comps/forms.py
+++ Comps/forms.py
@@ -1,8 +1,16 @@
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from Comps.models import Complaint
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ('username', 'password1', 'password2')  # Customize fields as needed
+
+
+class ComplaintForm(forms.ModelForm):
+    class Meta:
+        model = Complaint
+        fields = ['comp_assign', 'subject', 'complaint_type', 'Description']
+        widgets = {'comp_assign': forms.RadioSelect,
+                   }
