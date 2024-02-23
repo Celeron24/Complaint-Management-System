1f128bebac4c64e3ac0c1981d315ddc6c6503291
Hamza <hzahir47@gmail.com>
2024-02-23 15:53:00 +0500

    added model

--- Comps/models.py
+++ Comps/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Complaint(models.Model):
+    COMP_ASSIGN_CHOICES = [
+        ('Complaint', 'Complaint'),
+        ('Assignment', 'Assignment'),
+    ]
+
+    Comp_Assign = models.CharField(max_length=50)
+    Subject = models.CharField(max_length=200)
+    complaint_type = models.CharField(max_length=50)
+    Description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Ip address and computer name, username.
+
+    def __str__(self):
+        return self.Subject
--- Comps/views.py
+++ Comps/views.py
@@ -24,5 +24,5 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.success("You have successfully logged out")
+    messages.success(request, "You have successfully logged out")
     return redirect('login')
