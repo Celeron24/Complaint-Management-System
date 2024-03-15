b669a2111ce3f5aff0551b22e9a62ec49211d895
Hamza Zahir <hzahir47@gmail.com>
2024-03-15 06:50:52 +0500

    run

--- Comps/views.py
+++ Comps/views.py
@@ -6,21 +6,22 @@ from Comps.forms import ComplaintForm
 from .models import Complaint
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Max, Min
 
 
 def your_search_view(request):
     if request.user.is_authenticated:
         query = request.GET.get('q')
         if query:
+            # Ensure that only complaints belonging to the current user are retrieved
             searched_complaints = Complaint.objects.filter(
                 Q(id__icontains=query) |
                 Q(Comp_Assign__icontains=query) |
-                Q(Subject__icontains=query)
+                Q(Subject__icontains=query),
+                user=request.user
             )
         else:
-            searched_complaints = Complaint.objects.all()
-
+            # Also filter complaints by the current user
+            searched_complaints = Complaint.objects.filter(user=request.user)
         return render(request, 'search.html', {'searched_complaints': searched_complaints})
     else:
         # Handle the case where the user is not authenticated (optional)
@@ -33,7 +34,7 @@ def complaint(request):
             form = ComplaintForm(request.POST)
             if form.is_valid():
                 instance = form.save(commit=False)
-                instance.user = request.user
+                instance.user = request.user  # Ensure the complaint is associated with the current user
                 instance.save()
                 messages.success(request, 'Your complaint has been registered!')
                 return redirect('home')  # Redirect to the homepage after submitting the form
@@ -45,49 +46,33 @@ def complaint(request):
         messages.error(request, "You must be logged in to view this page.")
         return redirect('login.html')
 
-    # form = ComplaintForm()
-    # if request.method == 'POST':
-    #     form = ComplaintForm(request.POST)
-    #     if form.is_valid():
-    #         pass
-    # return render(request, "complaint.html", {'form': form})
-
 
 def home(request):
-    # valid_sort_fields = ['-status', 'created_at', 'id', 'Subject']
-    default_sort_field = '-status'
+    if request.user.is_authenticated:
+        default_sort_field = '-status'
 
-    # Get the sorting preference from the session or use the default
-    sort_by = request.GET.get('sort_by', default_sort_field)
+        sort_by = request.GET.get('sort_by', default_sort_field)
 
-    # Check if auto_sort parameter is present in the request
-    auto_sort = request.GET.get('auto_sort')
+        if sort_by == '-status':
+            all_complaints = Complaint.objects.filter(user=request.user).order_by('-status', 'created_at')
+        else:
+            all_complaints = Complaint.objects.filter(user=request.user).order_by(sort_by)
 
-    if auto_sort:
-        # Update the session with the current sorting preference
-        request.session['sort_by'] = sort_by
-    else:
-        # If auto_sort is not present, use the session sorting preference
-        sort_by = request.session.get('sort_by', default_sort_field)
-
-    # If sorting by status, move pending complaints to the first page
-    if sort_by == '-status':
-        all_complaints = Complaint.objects.order_by('-status', 'created_at')
         paginator = Paginator(all_complaints, 5)
+        page = request.GET.get('page')
+
+        try:
+            all_complaints = paginator.page(page)
+        except PageNotAnInteger:
+            all_complaints = paginator.page(1)
+        except EmptyPage:
+            all_complaints = paginator.page(paginator.num_pages)
+
+        context = {'all_complaints': all_complaints, 'sort_by': sort_by}
+        return render(request, 'home.html', context)
     else:
-        all_complaints = Complaint.objects.all().order_by(sort_by)
-        paginator = Paginator(all_complaints, 5)
-
-    page = request.GET.get('page')
-    try:
-        all_complaints = paginator.page(page)
-    except PageNotAnInteger:
-        all_complaints = paginator.page(1)
-    except EmptyPage:
-        all_complaints = paginator.page(paginator.num_pages)
-
-    context = {'all_complaints': all_complaints, 'sort_by': sort_by}
-    return render(request, 'home.html', context)
+        messages.error(request, "You must be logged in to view this page.")
+        return redirect('login')
 
 
 def login_user(request):
@@ -112,22 +97,12 @@ def logout_user(request):
     return redirect('login')
 
 
-# def search(request):
-#     if request.method == "POST":
-#         # Grab the form field input
-#         search = request.POST['search']
-#         # Search the database
-#         searched = Meep.objects.filter(body__contains=search)
-#
-#         return render(request, 'search.html', {'search': search, 'searched': searched})
-#     else:
-#         return render(request, 'search.html', {})
 def solvedcomplaints(request):
     if request.user.is_authenticated:
-        solved_complaints = Complaint.objects.filter(status=1)
+        solved_complaints = Complaint.objects.filter(status=1, user=request.user)
         return render(request, 'solvedcomplaints.html', {'solved_complaints': solved_complaints})
     else:
-        messages.error(request, "You have to Login First")
+        messages.error(request, "You must be logged in to view this page.")
         return redirect('login')
 
 
@@ -142,4 +117,4 @@ class ViewComplaint(DetailView):
     context_object_name = 'complaint'  # Optionally specify the context variable name
 
     def get_object(self, queryset=None):
-        return Complaint.objects.get(pk=self.kwargs['pk'])
+        return Complaint.objects.get(pk=self.kwargs['pk'], user=self.request.user)
